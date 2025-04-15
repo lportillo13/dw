d558c2c76c50ac27a7e3a5f9b014e5125e6865a8
lportillo13 <76447829+lportillo13@users.noreply.github.com>
2025-04-15 18:14:53 -0400

    adding database pages 1

--- pages/api/getCoupleDetails.js
+++ pages/api/getCoupleDetails.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     const { data, error } = await supabase
       .from('couples')
       .select('*')
-      .eq('id', Number(id))
+      .eq('couple_id', Number(id))
       .single();
 
     if (error) {
