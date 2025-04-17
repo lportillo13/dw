28ecbb83c03500a2a8ffcb1079712e54c1d349f0
lportillo13 <76447829+lportillo13@users.noreply.github.com>
2025-04-17 15:02:04 -0400

    Languages Implementation 2

---
+++ i18n.js
@@ -0,0 +1,9 @@
+// i18n.js
+module.exports = {
+    locales: ['es', 'en'],
+    defaultLocale: 'es',
+    pages: {
+      '*': ['common']
+    }
+  };
+  
--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 const nextTranslate = require('next-translate-plugin');
 
 module.exports = nextTranslate({
-  i18n: {
-    locales: ['es', 'en'],
-    defaultLocale: 'es',
-  },
+  reactStrictMode: true
 });
